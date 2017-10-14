4242679189567030da8c7427536290ca618cf4ba
George Antonious <george@antonious.ca>
2017-10-14 12:22:13 -0600

    Add camera controls

--- app/src/main/java/ca/antonious/tripod/MainActivity.java
+++ app/src/main/java/ca/antonious/tripod/MainActivity.java
@@ -1,5 +1,7 @@
 package ca.antonious.tripod;
 
+import android.database.Cursor;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private String getMostRecentPhotoUrl() {
+        String[] projection = new String[]{
+                MediaStore.Images.ImageColumns._ID,
+                MediaStore.Images.ImageColumns.DATA,
+                MediaStore.Images.ImageColumns.BUCKET_DISPLAY_NAME,
+                MediaStore.Images.ImageColumns.DATE_TAKEN,
+                MediaStore.Images.ImageColumns.MIME_TYPE
+        };
+
+        final Cursor cursor = getContentResolver()
+                .query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, projection, null,
+                        null, MediaStore.Images.ImageColumns.DATE_TAKEN + " DESC");
+        return cursor.getString(1);
     }
 }
